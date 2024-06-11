3db04d3dc7892ab27d68c41652dae741c06c0f63
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-06-11 22:57:58 +0530

    Hero added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-export default function Home() {
+import Hero from "@/components/Hero";
+import React from "react";
+
+const Home: React.FC = () => {
   return (
     <main className="min-h-screen bg-lime-100">
       <div className="h-screen w-full bg-lime-100 bg-grid-black/[0.2] relative flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-lime-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-          Backgrounds
-        </p>
+        <Hero />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
+
+const Hero: React.FC = () => {
+  return (
+    <HeroHighlight>
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: [20, -5, 0],
+        }}
+        transition={{
+          duration: 0.5,
+          ease: [0.4, 0.0, 0.2, 1],
+        }}
+        className="max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+      >
+        <Highlight className="text-xl md:text-3xl lg:text-4xl font-bold text-lime-900">
+          Innovative Solutions with JavaScript
+        </Highlight>
+        <div className="text-xl md:text-3xl lg:text-4xl pt-5">
+          <p>Welcome! I&apos;m Prasenjit Das</p>
+          <p>A JavaScript engineer</p>
+          <p className="text-xl">
+            Passionate about crafting cutting-edge web applications using the
+            latest technologies.
+          </p>
+          <p className="text-xl">
+            Let&apos;s build something amazing together.
+          </p>
+        </div>
+      </motion.h1>
+    </HeroHighlight>
+  );
+};
+
+export default Hero;
