ca422eba9409ffda64f67e23019c8d53b095377e
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-06-15 11:45:03 +0530

    Api deleted and og url added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,8 @@ export const metadata: Metadata = {
     title: "JavaScript Engineer Portfolio - Prasenjit Das",
     description:
       "Explore the portfolio of a skilled JavaScript engineer, showcasing web development projects, interactive websites, and front-end expertise.",
-    images: "http://localhost:3000/api/og",
+    images:
+      "https://og.tailgraph.com/og?fontFamily=Roboto&title=Innovative%20Solutions%20with%20JavaScript&titleTailwind=font-bold%20text-6xl%20text-lime-900&titleFontFamily=Dancing%2BScript&text=Welcome!%20I'm%20Prasenjit%20Das%2C%20%20A%20JavaScript%20engineer&textTailwind=text-2xl%20mt-4%20text-lime-700&textFontFamily=Inter&logoUrl=&logoTailwind=h-8%20bg-black&bgTailwind=bg-lime-100&footer=www.prasenjitpriyan.blog&footerTailwind=text-lime-900%20bg-lime-200&containerTailwind=bg-transparent&t=1718431946275&refresh=1",
   },
 };
 
